1dcd903a3c7d43fd42b54248540b6a9b3054f4b9
Jon Bell <github@lot23.com>
2023-09-17 10:38:25 +1200

    Fixing escaped stuff

--- pages/a-pattern-library/before-and-after-slider.js
+++ pages/a-pattern-library/before-and-after-slider.js
@@ -14,9 +14,9 @@ const BeforeAndAfterSlider = () => {
         <p>
           When a natural disaster strikes, one of the best ways to show the
           damage is one of those little before/after sliders. But usage of these
-          are uncommon because they're hard to make. So whether reading on a
-          website or an ebook, the best technology we have is to put two images
-          next to each other and make the user spot the differences.
+          are uncommon because they&apos;re hard to make. So whether reading on
+          a website or an ebook, the best technology we have is to put two
+          images next to each other and make the user spot the differences.
         </p>
       </div>
     </div>
