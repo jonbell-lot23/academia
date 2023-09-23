1fd977c760bcc13865c0072355bd3ee69a335959
Jon Bell <github@lot23.com>
2023-09-24 11:13:27 +1300

    Some recent changes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import replay from '@replayio/cypress';
 
 export default defineConfig({
+  projectId: 'yktsmq',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents: (on, config) => {
+      // Install the replay plugin
+      replay(on, config);
+
+      return config;
     },
-  baseUrl: 'http://localhost:3000',
   },
 });
-
---
+++ cypress/e2e/1-successful-load.cy.ts
@@ -0,0 +1,7 @@
+describe('Academia load test', () => {
+  it('should load academia.lot23 successfully', () => {
+    // Go to the home page
+    cy.visit('https://academia.lot23.com/');
+
+  });
+});
