53cd4a3e7be49c05616fda6f237cb2c64c2356e8
Jon Bell <github@lot23.com>
2023-09-24 11:52:36 +1300

    Fixing tests to work on this build

--- cypress/e2e/1-basic/load-pages-check-links.cy.js
+++ cypress/e2e/1-basic/load-pages-check-links.cy.js
@@ -1,5 +1,8 @@
 describe("Page and Links Check", () => {
-  const pages = ["/", "/paper", "/diff", "/a-pattern-library/last-read"];
+  const baseUrl = Cypress.env("BASE_URL") || "http://localhost:3000"; // Fallback to localhost if env variable not set
+  const pages = ["/", "/paper", "/diff", "/a-pattern-library/last-read"].map(
+    (page) => `${baseUrl}${page}`
+  );
 
   pages.forEach((page) => {
     it(`successfully loads ${page} and has no broken links`, () => {
@@ -8,7 +11,9 @@ describe("Page and Links Check", () => {
 
       // Links Check
       cy.get("a").each(($a) => {
-        cy.request($a.prop("href")).its("status").should("eq", 200);
+        const href = new URL($a.prop("href"));
+        const fullUrl = `${baseUrl}${href.pathname}${href.search}${href.hash}`;
+        cy.request(fullUrl).its("status").should("eq", 200);
       });
     });
   });
