5a7a89f3bb6f3c6f9f43157a69b687635adceba3
Jon Bell <github@lot23.com>
2023-10-02 17:47:38 +1300

    Cypress cloud stuff

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 import replay from '@replayio/cypress';
 
 export default defineConfig({
-  projectId: 'yktsmq',
+  projectId: '9xerqv',
   e2e: {
     setupNodeEvents: (on, config) => {
       // Install the replay plugin
