54fce66ce51fd0672105eca2d3f17325b91c4e94
Jon Bell <github@lot23.com>
2023-11-12 17:19:16 +1300

    Getting embedded logic

---
+++ components/SampleComponent.tsx
@@ -0,0 +1,11 @@
+const SampleComponent = () => {
+  const calculation = 9 + 2;
+
+  return (
+    <div className="p-4 bg-blue-200 border border-blue-800 rounded-sm">
+      The result of the calculation is: {calculation}
+    </div>
+  );
+};
+
+export default SampleComponent;
--- pages/thesis.tsx
+++ pages/thesis.tsx
@@ -1,30 +1,33 @@
-import React, { useEffect, useState } from "react";
-import ReactMarkdown from "react-markdown";
+import { serialize } from "next-mdx-remote/serialize";
+import { MDXRemote } from "next-mdx-remote";
+import SampleComponent from "../components/SampleComponent";
 import LeftNav from "../components/LeftNav";
 
-const Paper: React.FC = () => {
-  const [documentContent, setDocumentContent] = useState("");
+const components = { SampleComponent }; // Ensure this matches the component used in your MDX
 
-  useEffect(() => {
-    const fetchLatestDocument = async () => {
-      const res = await fetch(`/api/fetchVersion`);
-      const data = await res.json();
-
-      setDocumentContent(data.content);
-    };
-
-    fetchLatestDocument();
-  }, []);
-
+export default function ThesisPage({ source }) {
   return (
     <div style={{ marginLeft: "220px" }}>
       <LeftNav />
-
       <div className="container px-4 mx-auto mt-16 prose">
-        <ReactMarkdown>{documentContent}</ReactMarkdown>
+        <MDXRemote {...source} components={components} />
       </div>
     </div>
   );
-};
+}
 
-export default Paper;
+export async function getServerSideProps(context) {
+  // Determine the base URL
+  const protocol = context.req.headers["x-forwarded-proto"] || "http";
+  const host = context.req.headers.host;
+  const baseUrl = `${protocol}://${host}`;
+
+  // Fetch the latest document from your API
+  const res = await fetch(`${baseUrl}/api/fetchVersion`);
+  const data = await res.json();
+
+  // Serialize the content for MDX
+  const mdxSource = await serialize(data.content, { components });
+
+  return { props: { source: mdxSource } };
+}
