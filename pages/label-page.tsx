7af62d19e6192254614e3fb285990ea1b3ccd77b
Jon Bell <github@lot23.com>
2023-12-24 14:09:11 +1300

    Labels look better now

--- pages/label-page.tsx
+++ pages/label-page.tsx
@@ -1,35 +1,55 @@
 // pages/label-page.tsx
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 
 const LabelPage: React.FC = () => {
   const router = useRouter();
   const { url, label } = router.query;
+  const [showBadge, setShowBadge] = useState(true);
 
   return (
     <>
       {url && label ? (
-        <div className="badge">
-          You clicked a link that said "{decodeURIComponent(label as string)}"
-          to get here.
+        <>
+          {showBadge && (
+            <div className="text-sm rounded-md shadow-lg badge w-300">
+              You clicked a link that said "
+              {decodeURIComponent(label as string)}" to get here.
+              <span className="close" onClick={() => setShowBadge(false)}>
+                X
+              </span>
+            </div>
+          )}
           <iframe
             src={decodeURIComponent(url as string)}
             width="100%"
             height="100%"
           />
-        </div>
+        </>
       ) : null}
 
       <style jsx>{`
         .badge {
           background-color: #f2f2f2;
           padding: 10px;
-          text-align: center;
+          text-align: left;
+          margin: 20px;
           height: 100px;
+          position: fixed;
+          width: 300px;
+          box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.75);
+          font-size: 0.875rem;
+        }
+        .close {
+          position: absolute;
+          right: 10px;
+          top: 10px;
+          cursor: pointer;
         }
         iframe {
           width: 100%;
           height: 100vh;
+          margin: 0;
         }
       `}</style>
     </>
