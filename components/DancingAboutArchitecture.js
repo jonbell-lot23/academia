37de2516f3fefc610a383d33312f8b4aba0c1296
Jon Bell <github@lot23.com>
2024-01-24 17:21:43 +1300

    Nice changes

--- components/DancingAboutArchitecture.js
+++ components/DancingAboutArchitecture.js
@@ -30,45 +30,46 @@ const One = () => {
       {selectedFormat === "Casual academic format" ? (
         <div>
           <p>
-            What if this thesis would only make sense if you first watched a
-            five minute video? How might I author it to make the most sense to
-            you as a reader, perhaps consuming the media by reading a printed
-            out PDF over tea, with no access to the internet or a video player?
-            What would I do? What options would I have? One hundred years ago,
-            or even twenty, the answer was clear: your research should not rely
-            on anything that the printed page could not support. Text would
-            work, diagrams and images could, in some context, and everything
-            else would be banished to a footnote, where almost no one would read
-            it.
+            What if this thesis only made sense if you first watched a five
+            minute video? How might I author it to make the most sense to you as
+            a reader with no access to the internet or ability to play video?
+            What would I do? One hundred years ago, or even ten, the answer was
+            clear: research should not rely on anything that the printed page
+            could not support. Text, diagrams, and images were first class media
+            types, and everything else was banished to a footnote, where almost
+            no one would read it. The words were the main event, and any
+            supporting media was a novel distraction at best.
           </p>
           <p>
             Things are hardly better today. It's still considered a novelty and
             a nuisance to include any sort of multimedia when explaining
             concepts in academia. Not because other formats have nothing to
-            offer, but because it's too hard. As a result, we write with the
-            assumption that most people won't chase down our references, whether
-            hyperlinks to multimedia or actual footnotes squirrelled away at the
-            bottom of the document. This is untenable. Interactive
+            offer, but because it's too hard to author and read. As a result, we
+            write with the assumption that most people won't chase down our
+            references, whether hyperlinks to multimedia or actual footnotes
+            squirrelled away at the bottom of the document. This is untenable.
+            Text can only go so far, and the future of reading, demonstration,
+            and education is not more text with footnotes. Interactive
             visualisations, videos, and improved ways of consuming text all have
             a role to play, and will in the future.
           </p>
 
           <p>
-            This thesis explores that evolving landscape. It questions why,
-            despite the digital revolution, the experience of reading –
-            especially in academic and scholarly contexts – often remains
-            anchored to conventions developed for paper. It argues that we're at
-            an inspiring moment in humn history, where the potential of digital
-            augmentation can transform the reading experience into something
-            more dynamic, interactive, and effective, while still respectful of
-            the deep-rooted traditions of written research and communication.
-            Throughout this paper, we'll return to two key principles:
-            <b>seamless blending</b> and <b>concise enhancement</b>. The future
-            of reading isn't more external links to multimedia content but
-            instead an approach to communication that uses as little technology
-            as possible to enhance the reading experience, with no need for
-            external content. The future of the reading experience will be
-            seamless, consice, and far better than what we experience today.
+            This thesis explores this evolving landscape and expanding horizon.
+            It questions why, despite the digital revolution, the experience of
+            reading – especially in academic and scholarly contexts – often
+            remains anchored to conventions developed for paper. It argues that
+            we're at an inspiring moment in human history, where the potential
+            of digital augmentation can transform the reading experience into
+            something more dynamic, interactive, and effective, while still
+            respectful of the deep-rooted traditions of written research and
+            communication, and aware of the cognitive limitations of the human
+            mind. The future of reading isn't more external links to multimedia
+            content but instead an approach to communication that uses as little
+            technology as possible to enhance the reading experience, with no
+            need for external content. Throughout this paper, we'll return to
+            two key principles to guide us:
+            <b>seamless blending</b> and <b>concise enhancement</b>.
           </p>
 
           <p>
@@ -77,20 +78,19 @@ const One = () => {
             us from long-form reading. It's about enriching the standard reading
             experience, acknowledging that in a world where technology has
             changed nearly every aspect of our lives, the humble act of reading
-            deserves its own renaissance. Now, as an experiment, try clicking
-            the "explain like I'm five" link below to watch this text change.
+            deserves its own renaissance. As an example, try clicking the
+            "explain like I'm five" link below to watch this text change.
           </p>
         </div>
       ) : (
         <div>
           <p>
-            "Can you watch YouTube inside a book? No, and even if we could, it
-            would be pretty strange because watching a video is different from
-            reading a book. Reading is its own thing, but we can still make it
-            better. We can find ways to make reading more fun and interesting by
-            adding helpful magical bits into it. This will help people to learn
-            in new ways and have more fun when they read. They'll also learn
-            better, which is a good goal. That's what this paper is about!
+            Can you watch YouTube inside a book? No, and even if we could, it
+            would be pretty strange! Reading is its own thing, and we can make
+            it better by adding helpful things to it. This will help people to
+            learn in new ways and have more fun when they read. They'll also
+            learn better, which is a good goal. And that's what this paper is
+            about! 🎈
           </p>
         </div>
       )}
