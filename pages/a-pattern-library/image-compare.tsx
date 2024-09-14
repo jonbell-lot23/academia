5153a68e50434f9fdd8b22483040381aba9da9e0
Jon Bell <github@lot23.com>
2024-09-15 10:26:03 +1200

    Fixed the alignment issue

--- pages/a-pattern-library/image-compare.tsx
+++ pages/a-pattern-library/image-compare.tsx
@@ -23,22 +23,24 @@ export const BeforeAndAfterSlider: React.FC<BeforeAndAfterSliderProps> = ({
           format === "standalone" ? "px-4 mt-16" : ""
         } mx-auto prose`}
       >
-        <div className="mx-auto">
-          <ReactCompareSlider
-            itemOne={
-              <ReactCompareSliderImage
-                src="media/city-before.png"
-                alt="Black and white"
-              />
-            }
-            itemTwo={
-              <ReactCompareSliderImage
-                src="media/city-after.png"
-                alt="Color"
-              />
-            }
-          />
-        </div>
+        <div className="mx-auto compare-slider-container">
+  <ReactCompareSlider
+    itemOne={
+      <ReactCompareSliderImage
+        src="media/city-before.png"
+        alt="Black and white"
+        className="compare-image" // Add a class name for additional styling
+      />
+    }
+    itemTwo={
+      <ReactCompareSliderImage
+        src="media/city-after.png"
+        alt="Color"
+        className="compare-image" // Add a class name for additional styling
+      />
+    }
+  />
+</div>
       </div>
     </div>
   );
